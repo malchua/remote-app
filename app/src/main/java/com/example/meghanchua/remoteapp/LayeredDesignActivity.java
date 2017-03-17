45497f2d34b165a2a6de67a88293fce5df149baf
Wilson <w-tieu@hotmail.com>
2017-03-16 23:33:35 -0500

    added functionality for layered to go to different layers

--- app/src/main/java/com/example/meghanchua/remoteapp/LayeredDesignActivity.java
+++ app/src/main/java/com/example/meghanchua/remoteapp/LayeredDesignActivity.java
@@ -1,7 +1,9 @@
 package com.example.meghanchua.remoteapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LayeredDesignActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class LayeredDesignActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layered_design);
+    }
+
+    public void buttonOnClickRemote(View view) {
+        Intent remoteIntent = new Intent(LayeredDesignActivity.this, LayeredMain.class);
+        LayeredDesignActivity.this.startActivity(remoteIntent);
+    }
+
+    public void buttonOnClickGuide(View view) {
+        Intent guideIntent = new Intent(LayeredDesignActivity.this, LayeredGuide.class);
+        LayeredDesignActivity.this.startActivity(guideIntent);
+    }
+
+    public void buttonOnClickPlayback(View view) {
+        Intent playbackIntent = new Intent(LayeredDesignActivity.this, LayeredPlayback.class);
+        LayeredDesignActivity.this.startActivity(playbackIntent);
     }
 }
