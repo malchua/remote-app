8a9b260ccd66415e8c8f9c72591679d662eb865d
Meghan Chua <chua.meghan@gmail.com>
2017-03-28 14:54:44 -0500

    Modified iphone designs. Added functionality to panel button for activity_iphone_design.

--- app/src/main/java/com/example/meghanchua/remoteapp/IphoneDesignActivity.java
+++ app/src/main/java/com/example/meghanchua/remoteapp/IphoneDesignActivity.java
@@ -1,7 +1,9 @@
 package com.example.meghanchua.remoteapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class IphoneDesignActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class IphoneDesignActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iphone_design);
+    }
+
+    public void buttonOnClickIphoneDesign2(View view) {
+        Intent iphoneDesign2Intent = new Intent(IphoneDesignActivity.this, IphoneDesign2.class);
+        IphoneDesignActivity.this.startActivity(iphoneDesign2Intent);
+
     }
 }
